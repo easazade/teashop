2a9cc41214c9e4aa7a414051ea718d7079d3e3cf
alireza easazade <easazade@gmail.com>
2026-06-24 20:41:21 +0330

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { AboutPage } from './pages/AboutPage.tsx'
+import HomePage from './pages/HomePage.tsx'
 
 export default function App() {
-  return <div></div>
+  return (
+    <Routes>
+      <Route path={'/'} element={<HomePage />}></Route>
+      <Route path={'about'} element={<AboutPage />}></Route>
+    </Routes>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import { Navbar } from './components/Navbar.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar names={['Ali', 'Ahmad', 'Zahra']}></Navbar>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
