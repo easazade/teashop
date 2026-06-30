3c750a95fc58b4a04e8a4db17cbf9a7df5ef61ee
alireza easazade <easazade@gmail.com>
2026-06-30 14:15:36 +0330

    Update Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,11 @@
-export default function Logo() {
-  return <div>LOGO</div>
+import logoCompact from '../assets/logo-compact.png'
+import logo from '../assets/logo.png'
+
+type LogoProps = {
+  sizeType?: 'large' | 'compact' | 'small'
+}
+
+export default function Logo({ sizeType = 'large' }: LogoProps) {
+  if (sizeType == 'large') return <img src={logo} className={'h-14 w-auto'} />
+  if (sizeType == 'compact') return <img src={logoCompact} className={'h-14 w-auto'} />
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import { Languages, Menu, Search, ShoppingBasket } from 'lucide-react'
+import { Link } from 'react-router'
 import Logo from './Logo'
 
 export function Navbar() {
   return (
-    <nav className={'flex flex-row items-center gap-4 w-full bg-background h-16 shadow-xs p-8'}>
+    <nav className={'flex flex-row items-center gap-4 w-full bg-background h-20 shadow-xs p-8'}>
       <Menu size={20} />
       <div className={'md:absolute md:left-1/2 md:-translate-x-1/2'}>
-        <Logo />
+        <Logo sizeType="compact" />
       </div>
       <div className={'flex-1'}></div>
-      <span>Login</span>
+      <Link to={'/login'}>Login</Link>
       <Search size={20} />
       <Languages size={20} />
       <ShoppingBasket size={20} />
