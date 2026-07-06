9370c48ef17ce0c3d3c6c62fe928aeebb705e6cc
alireza easazade <easazade@gmail.com>
2026-07-06 12:18:53 +0330

    Add SearchBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
-import { Languages, Menu, Search, ShoppingBasket } from 'lucide-react'
+import { Languages, Menu, ShoppingBasket } from 'lucide-react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router'
 import CartOverlay from './Cart/CartOverlay'
 import Logo from './Logo'
+import { SearchBar } from './SearchBar'
 
 export function Navbar() {
   const [isCartOpen, setIsCartOpen] = useState(false)
@@ -11,12 +12,12 @@ export function Navbar() {
   return (
     <nav className={'flex flex-row items-center gap-4 w-full bg-background h-20 shadow-xs p-8'}>
       <Menu size={20} cursor={'pointer'} />
+      <SearchBar />
       <div className={'md:absolute md:left-1/2 md:-translate-x-1/2 cursor-pointer'} onClick={() => navigate('/')}>
         <Logo sizeType="compact" />
       </div>
       <div className={'flex-1'}></div>
       <Link to={'/login'}>Login</Link>
-      <Search size={20} />
       <Languages size={20} />
       <ShoppingBasket size={20} onClick={() => setIsCartOpen(true)} cursor={'pointer'} />
       <CartOverlay isOpen={isCartOpen} closeOverlay={() => setIsCartOpen(false)} />
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import { Search } from 'lucide-react'
+import React, { useState, type ReactElement } from 'react'
+
+export function SearchBar(): ReactElement {
+  const [text, setText] = useState('')
+
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value)
+    let processed = event.target.value
+    if (processed.startsWith('+')) {
+      processed = processed.substring(1)
+    }
+
+    setText(processed)
+  }
+
+  return (
+    <div
+      className={'flex items-center gap-2 border-b border-gray-300 pb-1 transition-colors focus-within:border-gray-900'}
+    >
+      <input
+        value={text}
+        onChange={handleInput}
+        placeholder="Search"
+        aria-label="Search"
+        className={`w-32 bg-transparent text-sm outline-none border-0 p-0
+         placeholder:text-gray-400 focus:border-0 focus:outline-none focus:ring-0 md:w-44`}
+      />
+      <Search size={20} className={'cursor-pointer transition-colors hover:text-gray-900'} />
+    </div>
+  )
+}
