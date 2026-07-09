27e446458e13851a402c3d37067d363392228147
Ali <easazade@gmail.com>
2026-07-09 16:21:26 +0330

    Make a slider

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -5,7 +5,13 @@ export default function HomePage() {
   return (
     <div className={'flex flex-col items-center'}>
       <Navbar />
-      <Slider />
+      <Slider
+        images={[
+          'https://images.pexels.com/photos/35652866/pexels-photo-35652866.jpeg',
+          'https://images.pexels.com/photos/37849928/pexels-photo-37849928.jpeg',
+          'https://images.pexels.com/photos/35004341/pexels-photo-35004341.jpeg',
+        ]}
+      />
     </div>
   )
 }
--- src/pages/Home/components/Slider.tsx
+++ src/pages/Home/components/Slider.tsx
@@ -1,3 +1,25 @@
-export function Slider() {
-  return <div className={'h-148 w-full bg-placeholder'}></div>
+import { useEffect, useState } from 'react'
+
+type SliderProps = {
+  images?: string[]
+}
+
+export function Slider({ images = [] }: SliderProps) {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setCurrentIndex((current) => (current + 1) % images.length)
+    }, 2500)
+
+    return () => {
+      window.clearInterval(intervalId)
+    }
+  }, [images.length])
+
+  return (
+    <div className={'h-148 w-full bg-placeholder overflow-hidden'}>
+      <img key={images[currentIndex]} src={images[currentIndex]} alt={''} className={'h-full w-full object-cover'} />
+    </div>
+  )
 }
