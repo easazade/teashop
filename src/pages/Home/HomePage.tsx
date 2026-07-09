4b94dfcc363bbd32fc7d4050352d112afacbfd46
Ali <easazade@gmail.com>
2026-07-09 16:33:27 +0330

    Fix small screen sizing issue

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -10,6 +10,7 @@ export default function HomePage() {
           'https://images.pexels.com/photos/35652866/pexels-photo-35652866.jpeg',
           'https://images.pexels.com/photos/37849928/pexels-photo-37849928.jpeg',
           'https://images.pexels.com/photos/35004341/pexels-photo-35004341.jpeg',
+          'https://images.pexels.com/photos/38166104/pexels-photo-38166104.jpeg'
         ]}
       />
     </div>
--- src/pages/Home/components/Slider.tsx
+++ src/pages/Home/components/Slider.tsx
@@ -8,6 +8,8 @@ export function Slider({ images = [] }: SliderProps) {
   const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
+    if (images.length <= 1) return
+
     const intervalId = window.setInterval(() => {
       setCurrentIndex((current) => (current + 1) % images.length)
     }, 2500)
@@ -18,8 +20,10 @@ export function Slider({ images = [] }: SliderProps) {
   }, [images.length])
 
   return (
-    <div className={'h-148 w-full bg-placeholder overflow-hidden'}>
-      <img key={images[currentIndex]} src={images[currentIndex]} alt={''} className={'h-full w-full object-cover'} />
+    <div className={'h-80 md:h-172 w-full bg-placeholder overflow-hidden'}>
+      {images[currentIndex] && (
+        <img key={images[currentIndex]} src={images[currentIndex]} alt={''} className={'h-full w-full object-cover'} />
+      )}
     </div>
   )
 }
