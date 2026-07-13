3669c6afa1039c50bbf57ec05d0e9d5407679e58
Ali <easazade@gmail.com>
2026-07-13 17:58:21 +0330

    Working on Deals component

--- src/components/Deals.tsx
+++ src/components/Deals.tsx
@@ -1,12 +1,19 @@
+import ShopItem from './ShopItem'
+
 export default function Deals() {
   const itemsImage = 'https://teashop.com/cdn/shop/files/minitines_2.png?v=1782824733&width=1254'
 
   return (
-    <div className="flex h-172 w-full flex-row items-center gap-8 overflow-hidden">
-      <div className="flex h-full min-w-0 flex-1 basis-0 items-center justify-center">
-        <img src={itemsImage} alt="Mini tea tins" className="max-h-full w-full object-cover" />
+    <div className="flex h-168 w-full flex-row items-center gap-8 overflow-hidden">
+      <div className="flex-1 basis-0">
+        <img src={itemsImage} className="object-cover" />
       </div>
-      <div className="flex h-full min-w-0 flex-1 basis-0 items-center justify-center"></div>
+      <div className="flex flex-1 basis-0 flex-col items-center">
+        <h3 className="mb-0">Buy 5 Mini Tea Tins, pay for 4! New flavours</h3>
+        <div className="mb-8 text-sm">Choose from irresistible flavours and create your perfect selection</div>
+
+        <ShopItem />
+      </div>
     </div>
   )
 }
--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -4,7 +4,10 @@ import Rating from './Rating'
 export default function ShopItem() {
   return (
     <div className="bg-surface relative flex h-auto min-w-80 flex-col items-center px-4 pt-2 pb-4">
-      <img src="https://teashop.com/cdn/shop/files/10156_ChaiLatte_1000x1000_l.jpg?v=1772541329&width=1000" />
+      <img
+        src="https://teashop.com/cdn/shop/files/10156_ChaiLatte_1000x1000_l.jpg?v=1772541329&width=1000"
+        className="max-w-84"
+      />
       <AppButton
         label={'Add to cart'}
         onClick={() => {
