df31817bbf39b966f3a79a4352da5493ce6513d7
Ali <easazade@gmail.com>
2026-07-18 20:15:23 +0330

    Fix CartOverlay only appearing on top and allowing scroll for below elements

--- src/components/Cart/CartOverlay.tsx
+++ src/components/Cart/CartOverlay.tsx
@@ -1,13 +1,26 @@
+import { useEffect } from 'react'
+import { cn } from '../../utils/functions'
+
 type CartOverlayProps = {
   isOpen: boolean
   closeOverlay: () => void
 }
 
 export default function CartOverlay({ isOpen, closeOverlay }: CartOverlayProps) {
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? 'hidden' : 'auto'
+
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [isOpen])
+
   return (
     <div
-      className={`z-60 flex flex-row justify-around
-        ${isOpen ? 'pointer-events-auto absolute top-0 right-0 bottom-0 left-0' : 'pointer-events-none hidden'} `}
+      className={cn(
+        'z-60 flex flex-row justify-around overflow-y-auto',
+        isOpen ? 'pointer-events-auto fixed top-0 right-0 bottom-0 left-0' : 'pointer-events-none hidden'
+      )}
     >
       <div className={'h-full flex-1 bg-white/10 backdrop-blur-xs'} onClick={closeOverlay}></div>
       <div className={'bg-surface h-full flex-1 p-4 shadow-lg'}>Cart Sidebar</div>
