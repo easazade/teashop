b544fad01d13187b9630755c6caff6c274a8585c
Ali <easazade@gmail.com>
2026-07-18 14:03:25 +0330

    Update AppButton to supported inverted style

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -1,20 +1,26 @@
 import type { CSSProperties, MouseEventHandler } from 'react'
+import { cn } from '../utils/functions'
 
 type AppButtonProps = {
   label: string
   onClick: MouseEventHandler<HTMLButtonElement>
   minWidth?: CSSProperties['minWidth']
+  inverted?: boolean
 }
 
-export default function AppButton({ label, onClick, minWidth = 100 }: AppButtonProps) {
+export default function AppButton({ label, onClick, minWidth = 100, inverted = false }: AppButtonProps) {
   return (
     <button
       type="button"
       onClick={onClick}
       // in below line you see javascript object short hand feature. complete version is {minWidth: minWidth}
       style={{ minWidth }}
-      className="bg-button text-button-text border-button hover:text-button cursor-pointer border-3 p-2 text-sm
-        transition-colors hover:bg-transparent"
+      className={cn(
+        inverted
+          ? 'bg-on-primary text-primary border-on-primary hover:text-on-primary'
+          : 'bg-primary text-on-primary border-primary hover:text-primary',
+        'cursor-pointer border-3 p-2 text-sm transition-colors hover:bg-transparent'
+      )}
     >
       {label.toUpperCase()}
     </button>
