0a0a54ee72421a1c046f3bf53c0e7a18b0688299
Ali <easazade@gmail.com>
2026-07-21 14:38:03 +0330

    Working on AppInput

--- src/components/AppInput.tsx
+++ src/components/AppInput.tsx
@@ -1,22 +1,29 @@
-import { type ComponentPropsWithoutRef } from 'react'
+import { type ComponentPropsWithoutRef, type ReactNode } from 'react'
 import { cn } from '../utils/functions'
 
 type AppInputProps = ComponentPropsWithoutRef<'input'> & {
   error?: string
+  minWidth?: number
+  children?: ReactNode
 }
 
-export function AppInput({ error, className, ...inputProps }: AppInputProps) {
+export function AppInput({ error, children, minWidth, className, value, ...inputProps }: AppInputProps) {
   return (
     <div className="flex flex-col gap-1.5">
-      <input
-        {...inputProps}
-        className={cn(
-          'border px-3 py-2 text-sm transition-colors outline-none placeholder:text-gray-400',
-          error ? 'border-error-600 focus:border-er' : 'focus:border-primary border-gray-300',
-          className
-        )}
-      />
-      {error && <span className="text-error-600 text-xs">{error}</span>}
+      <div className="relative">
+        <input
+          {...inputProps}
+          value={value}
+          style={{ minWidth }}
+          className={cn(
+            'placeholder:text-gray-text border px-3 py-2.5 text-sm transition-colors outline-none',
+            error ? 'border-error focus:border-error' : 'focus:border-primary border-gray-300',
+            className
+          )}
+        />
+        {!value && <div className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer leading-0">{children}</div>}
+      </div>
+      {error && <span className="text-error text-xs">{error}</span>}
     </div>
   )
 }
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,16 +1,39 @@
+import { useState } from 'react'
 import AppButton from '../../components/AppButton'
 import AppInput from '../../components/AppInput'
 import { PageLayout } from '../PageLayout'
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <PageLayout title="Login">
+    <PageLayout title="Login" className="gap-4 p-54">
       <h1>Login</h1>
-      <p className="mt-6 mb-6 text-sm font-light">Enter your email and password to login:</p>
-      <AppInput placeholder={'E-mail'} />
-      <AppInput placeholder={'Password'} />
-      <AppButton label="Login" onClick={() => {}} />
-      <span>Don't have an account? Sign up</span>
+      <p className="mt-2 mb-2 text-sm font-light">Enter your email and password to login:</p>
+      <AppInput
+        placeholder={'E-mail'}
+        minWidth={420}
+        value={email}
+        onChange={(input) => setEmail(input.target.value)}
+      />
+      <AppInput
+        placeholder={'Password'}
+        minWidth={420}
+        value={password}
+        onChange={(input) => setPassword(input.target.value)}
+      >
+        <span className="text-xs">Forgot your password?</span>
+      </AppInput>
+      <AppButton
+        label="Login"
+        minWidth={420}
+        className="mt-2"
+        onClick={() => {
+          alert(`Email is ${email} & Password is ${password}`)
+        }}
+      />
+      <span className="text-primary-light text-sm font-light">Don't have an account? Sign up</span>
     </PageLayout>
   )
 }
