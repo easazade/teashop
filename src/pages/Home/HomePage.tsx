80612d206eda16ea9a48291ddc247f67284e8344
Ali <easazade@gmail.com>
2026-07-21 11:19:41 +0330

    Remove unneeded VerticalGap components

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,5 @@
 import BestSellers from '../../components/BestSellers'
 import Deals from '../../components/Deals'
-import { VerticalGap } from '../../components/Gap'
 import { OurHistory } from '../../components/OurHistory'
 import VisitStores from '../../components/VisitStores'
 import { PageLayout } from '../PageLayout'
@@ -9,17 +8,12 @@ import { Slider } from './components/Slider'
 
 export default function HomePage() {
   return (
-    <PageLayout>
+    <PageLayout className="gap-8">
       <Slider />
-      <VerticalGap vertical={30} />
       <ExploreCollections />
-      <VerticalGap vertical={30} />
       <BestSellers />
-      <VerticalGap vertical={30} />
       <Deals />
-      <VerticalGap vertical={30} />
       <VisitStores />
-      <VerticalGap vertical={30} />
       <OurHistory />
     </PageLayout>
   )
